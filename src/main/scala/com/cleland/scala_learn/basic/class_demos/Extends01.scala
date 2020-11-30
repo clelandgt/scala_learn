b15568e61373c6e2e8167a292ddb0032b8e1e1c5
cleland <clelandgt@163.com>
2020-11-30 08:03:03 +0800

    UPD: Extends01

--- src/main/scala/com/cleland/scala_learn/basic/class_demos/Extends01.scala
+++ src/main/scala/com/cleland/scala_learn/basic/class_demos/Extends01.scala
@@ -1,31 +1,37 @@
 package com.cleland.scala_learn.basic.class_demos
 
+//object Extends01 {
+//  def main(args: Array[String]): Unit = {
+//    val stu = new Student("Hua Xiao", 15)
+//    stu.studying()
+//
+//  }
+//}
+//
+//class Person(name1: String, age1: Int){
+//  val name: String = name1
+//  val age: Int = age1
+//
+//  def showInfo(): Unit = {
+//    println("学生信息如下")
+//    println(s"名字: ${this.name}   年龄: ${this.age}")
+//  }
+//
+////  def this(name: String, age: Int){
+////    this(name, age)
+////  }
+//
+//}
+//
+//class Student(name1: String, age1: Int) extends Person(name1: String, age1: Int) {
+//  def studying(): Unit = {
+//    println(s"${this.name} 学习中....")
+//  }
+//
+//}
+
 object Extends01 {
   def main(args: Array[String]): Unit = {
-    val stu = new Student("Hua Xiao", 15)
-    stu.studying()
-
+    println("hello")
   }
-}
-
-class Person(name1: String, age1: Int){
-  val name: String = name1
-  val age: Int = age1
-
-  def showInfo(): Unit = {
-    println("学生信息如下")
-    println(s"名字: ${this.name}   年龄: ${this.age}")
-  }
-
-//  def this(name: String, age: Int){
-//    this(name, age)
-//  }
-
-}
-
-class Student(name1: String, age1: Int) extends Person(name1: String, age1: Int) {
-  def studying(): Unit = {
-    println(s"${this.name} 学习中....")
-  }
-
 }
